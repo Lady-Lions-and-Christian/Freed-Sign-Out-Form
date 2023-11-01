954151fa8131edeb1dcb7b12d941d99d5b5a089a
kayleeodom <kaylee.odom@students.fhu.edu>
2023-11-01 16:51:11 -0500

    cleaned up username/ took Christians work

--- scripts.js
+++ scripts.js
@@ -16,34 +16,6 @@ formSubmission.addEventListener("submit", (e) => {
 
 })
 
-// 
-// function authenticate(formData) {
-//     const enteredUsername = formData.username;
-//     const enteredPassword = formData.password;
-//     const message = document.getElementById("message");
-  
-//     // Read the JSON file with user data
-//     fetch("./signout.json")
-//       .then(response => response.json())
-//       .then(data => {
-//         const users = data;
-//         const user = users.find(u => u.username === enteredUsername && u.password === enteredPassword);
-  
-//         if (user) {
-//           // Authentication successful
-//           message.textContent = "Login successful!";
-//           // Redirect to another page or perform other actions here
-//         } else {
-//           // Authentication failed
-//           message.textContent = "Invalid username or password. Please try again.";
-//         }
-//       })
-//       .catch(error => {
-//         console.error("Error reading JSON file: " + error);
-//         message.textContent = "An error occurred while processing your request.";
-//       });
-// }
-
 /*Traveling With Functionality*/
 const optionalBlank = document.querySelector("#travel-select-optional");
 const choiceSelect = document.querySelector("#travel-select")
